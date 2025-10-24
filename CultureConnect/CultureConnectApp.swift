395d6c96d4e4dbd99356539a803d7c6b8925d25b
4029371 <s4029371@student.rmit.edu.au>
2025-10-24 21:41:36 +1100

    Initial Commit

---
+++ CultureConnect/CultureConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CultureConnectApp.swift
+//  CultureConnect
+//
+//  Created by Donovan Ong on 24/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct CultureConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CultureConnectTests/CultureConnectTests.swift
@@ -0,0 +1,17 @@
+//
+//  CultureConnectTests.swift
+//  CultureConnectTests
+//
+//  Created by Donovan Ong on 24/10/2025.
+//
+
+import Testing
+@testable import CultureConnect
+
+struct CultureConnectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
